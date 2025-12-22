5c18f71e3eb5a53faa7746c51e57032804d630c8
Karol <93782725+petitess@users.noreply.github.com>
2025-12-22 12:07:12 +0100

    string

---
+++ xxx01/string.py
@@ -0,0 +1,13 @@
+# winter = input('Do you like winter? ')
+price = 10
+rating = 4.9
+season = "winter"
+print(season.find("w"))
+print("win" in season)
+is_published = True
+print(season.title())
+print(f"price: {price}")
+price = 20
+print(f"price: {price}")
+print(f'{season.title()} has {len(season)} characters')
+print("*" *  10)
