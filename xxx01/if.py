a9652034eccbb84c85946bb603c2e657f0ba5975
Karol <93782725+petitess@users.noreply.github.com>
2025-12-22 13:06:12 +0100

    if, while

--- xxx01/if.py
+++ xxx01/if.py
@@ -18,3 +18,22 @@ if temperature > 30:
     print("It's a hot day")
 else:
     print("It's not a hot day")
+
+i = 1 
+
+while i <= 5:
+    print(i)
+    print( '*' * i)
+    i = i + 1
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guesss: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You won')
+        break
+else:
+    print('Sorry you failed') 
