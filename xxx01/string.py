6efd2c8eadc877e40c96c31583e71b88f82ffb35
Karol <93782725+petitess@users.noreply.github.com>
2025-12-23 10:22:09 +0100

    string, tuple unpacking, dictionary
    Added tuple unpacking and customer birthdate.

--- xxx01/string.py
+++ xxx01/string.py
@@ -11,3 +11,11 @@ price = 20
 print(f"price: {price}")
 print(f'{season.title()} has {len(season)} characters')
 print("*" *  10)
+
+tuple = (1, 3, 6)
+x, y, z = tuple
+print(x, y, z)
+
+customer = {"name": "John Smith", "Age": 30, "is_verfiied": True}
+customer["birthdate"] = "Jan 1 1980"
+print(customer)
