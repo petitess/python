e3f75db9b96a255d13ccd08be96034b9390be7d6
Karol <93782725+petitess@users.noreply.github.com>
2026-01-05 12:54:08 +0100

    get file content, wildcard, loop
    Added functionality to log JSON files created today.

--- xxx01/file_content.py
+++ xxx01/file_content.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 items = os.listdir('c:/temp') 
@@ -6,4 +7,13 @@ for item in items:
 
 with open('c:/temp/json.json', 'r', encoding='utf-8') as file:
     content = file.read()
-    print(content)
+    # print(content)
+
+year = datetime.datetime.now().strftime("%Y-%m-%d")
+for item in os.listdir('c:/temp'):
+    if item.endswith('.json') and item.startswith(year):
+        print("Log: ", item)
+        with open(f'c:/temp/{item}', 'r', encoding='utf-8') as file:
+            get_logs = file.read()
+
+# print(get_logs)
